9a32847cc6dfaa55ae7527f1029eb063ef108a25
Christian Hardmeier <ch@rax.ch>
2013-02-08 15:56:39 +0100

    Fixed encoding problems in dump_model.

--- nn6/dump_model.m
+++ nn6/dump_model.m
@@ -1,12 +1,12 @@
-function dump_model(outstem, net, W)
+function dump_model(outstem, net, voc, W)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    id = fopen(strcat(outstem, '.nn6.srcvoc'), 'w');
+    id = fopen(strcat(outstem, '.nn6.srcvoc'), 'w', 'n', 'UTF-8');
     fprintf(id, '%s\n', net.srcvoc{:});
     fclose(id);
 
-    id = fopen(strcat(outstem, '.nn6.tgtvoc'), 'w');
-    fprintf(id, '%s\n', net.srcvoc{:});
+    id = fopen(strcat(outstem, '.nn6.tgtvoc'), 'w', 'n', 'UTF-8');
+    fprintf(id, '%s\n', net.tgtvoc{:});
     fclose(id);
 
     if isfield(net, 'tgtprons')
@@ -15,7 +15,7 @@ function dump_model(outstem, net, W)
         tgtprons = {{'ce','c'''},{'elle'},{'elles'},{'il'},{'ils'}};
     end
     
-    id = fopen(strcat(outstem, '.nn6.classes'), 'w');
+    id = fopen(strcat(outstem, '.nn6.classes'), 'w', 'n', 'UTF-8');
     for i = 1:length(tgtprons)
         fprintf(id, '%s ', tgtprons{i}{1:end-1});
         fprintf(id, '%s\n', tgtprons{i}{end});
@@ -34,7 +34,7 @@ function dump_model(outstem, net, W)
     % - hidden layer size
     % - output layer size
     
-    id = fopen(strcat(outstem, '.nn6.model'), 'w');
+    id = fopen(strcat(outstem, '.nn6.model'), 'w', 'n', 'UTF-8');
     header = [hex2dec('2fed70b9') 6 1 ...
                 net.srcngsize length(net.srcvoc) length(net.tgtvoc) ...
                 net.srcembed net.antembed net.hidden net.output];
@@ -44,6 +44,11 @@ function dump_model(outstem, net, W)
     fwrite(id, W.antembed, 'float32');
     fwrite(id, W.embhid, 'float32');
     fwrite(id, W.hidout, 'float32');
-    fclose(id);  
+    fclose(id);
+    
+    coref_file = strcat(outstem, '.nn6.coref');
+    coref = struct('features', find(voc.activelinkfeat), 'Ahid', net.Ahid, ...
+        'linkAhid', W.linkAhid, 'AhidAres', W.AhidAres');
+    save(coref_file, '-struct', 'coref', '-ascii', '-tabs');
 end
 
