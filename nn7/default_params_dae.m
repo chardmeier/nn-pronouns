feb3ab78b34a11866b183be90c23a4c44f6d4b12
Christian Hardmeier <ch@rax.ch>
2013-03-19 15:49:06 +0100

    Added default_params_dae.m.

---
+++ nn7/default_params_dae.m
@@ -0,0 +1,18 @@
+function params = default_params_dae
+    params = struct();
+
+    params.nsteps = 100;
+    params.stddev = .01;
+    params.momentum = 0.9;
+    params.alpha = .001;
+    params.alphadecay = 1;
+    params.batchsize = 50;
+    params.adjust_rate = true;
+    params.early_stopping = true;
+    params.noise_threshold = .5;
+    params.l2regulariser = 1e-4;
+    
+    params.F1 = @(x) 1 ./ (1 + exp(-x));
+    params.DF1 = @(x) x .* (1 - x);
+    params.F2 = @(x) 1 ./ (1 + exp(-x));
+end
