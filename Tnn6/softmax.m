a0074e63692212b0694d60dd0991c86cdaf374a8
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2014-03-26 23:58:49 +0100

    Bugfix in softmax functions.

--- Tnn6/softmax.m
+++ Tnn6/softmax.m
@@ -2,7 +2,7 @@ function out = softmax(in, varargin)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     if nargin > 1 && strcmp(varargin{1}, 'addcategory')
-        in2 = [in, ones(size(in, 1), 1)];
+        in2 = [in, zeros(size(in, 1), 1)];
     else
         in2 = in;
     end
